ac39be75f1d78af4fc59c67060223cf63530e63d
at <at@PC-3.mshome.net>
2020-11-05 18:13:08 +0100

    Cambios. Fase dos.

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,7 +5,12 @@ public class Principal
 
     public static void main(String[] args)
     {
-        System.out.println("klklklkkklklklklklklklklklklklkk");
+        System.out.println("klk manin");
+        System.out.println("significa killer latin king manin");
+        System.out.println("manin esta jodido");
+
+
+
     }
 
 }
